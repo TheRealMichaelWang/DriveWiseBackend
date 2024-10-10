7addf333815a47f978b2dbab285d5052e9bd20dc
TheRealMichaelWang <mwang764@gatech.edu>
2024-10-09 22:03:29 -0400

    * Added support for loading accidents

--- DriveWise.h
+++ DriveWise.h
@@ -3,6 +3,10 @@
 namespace DriveWise {
 	class accident {
 	private:
-		
+		double latitude;
+		double longitude;
+
+	public:
+		accident(double latitude, double longitude) : latitude(latitude), longitude(longitude) { }
 	};
 }
--- DriveWiseBackend.cpp
+++ DriveWiseBackend.cpp
@@ -2,11 +2,33 @@
 //
 
 #include <iostream>
+#include <vector>
+#include <cassert>
+#include "DriveWise.h"
+#include "rapidcsv.h"
 
 using namespace std;
 
+static std::vector<DriveWise::accident> accidents;
+
+void load_accidents() {
+	std::cout << "Loading accidents...";
+	
+	rapidcsv::Document accidentList("lat_long.csv");
+
+	std::vector<double> xCoords = accidentList.GetColumn<double>("XCOORD");
+	std::vector<double> yCoords = accidentList.GetColumn<double>("YCOORD");
+
+	assert(xCoords.size() == yCoords.size());
+	accidents.reserve(xCoords.size());
+	for (size_t i = 0; i < xCoords.size(); i++) {
+		accidents.push_back(DriveWise::accident(xCoords.at(i), yCoords.at(i)));
+	}
+}
+
 int main()
 {
-	cout << "Hello CMake." << endl;
+	load_accidents();
+
 	return 0;
 }
